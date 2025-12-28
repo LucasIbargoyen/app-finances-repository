cb1b75208f4ac591189e7e7536745c0278ca0108
Lucas Ortiz <lukkaskolish@outlook.com>
2025-12-28 19:33:27 -0300

    Done: main.py with user data, and financial data, and a simple index.html which is gonna be worked later

---
+++ main.py
@@ -0,0 +1,8 @@
+"""Dados pessoais de usuário"""
+user = input("What's your name? ")
+birtDate = int(input("Digit your birthday: "))
+email = input("Digit your e-mail: ")
+cpf = input("CPF: ")
+"""Dados Financeiros"""
+avgIncome = float(input("Digit your income: "))
+avgExpenses = float(input("Digit your expenses: "))
