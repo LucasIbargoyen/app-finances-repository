2c99f39e916bae9fd5dbd98dcfd22280f1ee39fb
Lucas Ortiz <lukkaskolish@outlook.com>
2026-04-23 18:11:22 -0300

    made the getters and setters

--- appfinances/src/main/java/com/app/model/Goal.java
+++ appfinances/src/main/java/com/app/model/Goal.java
@@ -6,4 +6,30 @@ public class Goal {
     private Double targetAmount;
     private Double currentAmount;
     private LocalDate deadLine;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public Double getTargetAmount() {
+        return targetAmount;
+    }
+    public void setTargetAmount(Double targetAmount) {
+        this.targetAmount = targetAmount;
+    }
+    public Double getCurrentAmount() {
+        return currentAmount;
+    }
+    public void setCurrentAmount(Double currentAmount) {
+        this.currentAmount = currentAmount;
+    }
+    public LocalDate getDeadLine() {
+        return deadLine;
+    }
+    public void setDeadLine(LocalDate deadLine) {
+        this.deadLine = deadLine;
+    }
+
 }
--- appfinances/src/main/java/com/app/model/Transaction.java
+++ appfinances/src/main/java/com/app/model/Transaction.java
@@ -12,4 +12,41 @@ public class Transaction {
     private TransactionType type;
     private Category category;
     
+    public String getUuid() {
+        return uuid;
+    }
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getAmount() {
+        return amount;
+    }
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    public TransactionType getType() {
+        return type;
+    }
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
 }
